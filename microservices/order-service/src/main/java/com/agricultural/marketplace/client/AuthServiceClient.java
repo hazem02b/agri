8c28918e5d95e62a812b79c2a56ae3bc806dbe1a
hazem <hazembellili80@gmail.com>
2026-04-12 17:01:48 +0100

    DevOPS: Prepare Docker configuration for new microservices ecosystem

--- microservices/order-service/src/main/java/com/agricultural/marketplace/client/AuthServiceClient.java
+++ microservices/order-service/src/main/java/com/agricultural/marketplace/client/AuthServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "auth-service", url = "http://localhost:8081")
+@FeignClient(name = "auth-service", url = "${auth-service.url:http://localhost:8081}")
 public interface AuthServiceClient {
 
     @GetMapping("/api/users")
